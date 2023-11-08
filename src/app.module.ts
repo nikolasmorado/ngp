2bdb52974112f1505277d86fab4edba615468fd5
Nikolas Morado <nikolasmorado@gmail.com>
2023-11-08 18:27:34 -0500

    GraphQL + PostgreSQL setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		GraphQLModule.forRootAsync<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			useFactory: () => ({
+				autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+				installSubscriptionHandlers: false,
+				sortSchema: true,
+				context: ({ req }) => ({ req }),
+				cache: 'bounded',
+				playground: true,
+			}),
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get<string>('DB_HOST'),
+				port: configService.get<number>('DB_PORT'),
+				username: configService.get<string>('DB_USERNAME'),
+				password: configService.get<string>('DB_PASSWORD'),
+				database: configService.get<string>('DB_DATABASE'),
+				synchronize: true,
+			}),
+		}),
+	],
+	providers: [AppResolver],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	const configService = app.select(AppModule).get(ConfigService);
+	await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
